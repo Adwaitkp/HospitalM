e0f523d8746d5f891c57ac3ea1a31bd4d50fcb1a
Adwaitkp <143967286+Adwaitkp@users.noreply.github.com>
2025-12-04 11:44:08 +0530

    Update MongoDB

--- hospital-backend/server.js
+++ hospital-backend/server.js
@@ -16,7 +16,7 @@ app.use(passport.initialize());
 
 // ✅ MongoDB Connection
 mongoose
-  .connect("mongodb+srv://adwaitlkshs:dqgMpLOgvYnqvh9X@cluster0.qbv5xha.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+  .connect("mongodb+srv://adwaitlkshs:dqgMpLOgvYnqvh9X@cluster0.qbv5xha.mongodb.net/?appName=Cluster0", {
     // Removed deprecated options
   })
   .then(() => console.log("✅ Connected to MongoDB"))
